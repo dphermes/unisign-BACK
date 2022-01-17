aa65be17a933e4716ec18d1fb05f1d1c54f79319
damienpuaud <damien.puaud@gmail.com>
2022-01-17 09:26:20 +0100

    Initial commit

---
+++ src/main/java/fr/kmcl/unisignBACK/UnisignBackApplication.java
@@ -0,0 +1,13 @@
+package fr.kmcl.unisignBACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisignBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnisignBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/kmcl/unisignBACK/UnisignBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kmcl.unisignBACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisignBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
