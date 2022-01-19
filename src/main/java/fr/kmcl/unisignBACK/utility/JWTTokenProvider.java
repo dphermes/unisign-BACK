7391699b1a5372590e215c46f6f9052a4202e8af
damienpuaud <damien.puaud@gmail.com>
2022-01-19 10:33:31 +0100

    CREATE - Jwt Token Verifier

--- src/main/java/fr/kmcl/unisignBACK/utility/JWTTokenProvider.java
+++ src/main/java/fr/kmcl/unisignBACK/utility/JWTTokenProvider.java
@@ -1,15 +1,22 @@
 package fr.kmcl.unisignBACK.utility;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import fr.kmcl.unisignBACK.constant.SecurityConstant;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import fr.kmcl.unisignBACK.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.auth0.jwt.algorithms.Algorithm.*;
 import static fr.kmcl.unisignBACK.constant.SecurityConstant.*;
+import static java.util.Arrays.stream;
 
 /**
  * @author KMCL (https://www.kmcl.fr)
@@ -30,7 +37,34 @@ public class JWTTokenProvider {
                 .sign(HMAC512(secret.getBytes()));
     }
 
-    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
-        return null;
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(KMCL_SAS).build();
+        } catch (JWTVerificationException exception) {
+            /* Don't throw the actual exception for security matters
+            ** Just send a custom message */
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal user) {
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : user.getAuthorities()) {
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
     }
 }
