e3714732a64807104be70e4e7e0c3ef284a66079
damienpuaud <damien.puaud@gmail.com>
2022-01-19 12:31:25 +0100

    CREATE - JWT Access Denied handler to send 401 Access Denied custom message

--- src/main/java/fr/kmcl/unisignBACK/filter/JwtAccessDeniedHandler.java
+++ src/main/java/fr/kmcl/unisignBACK/filter/JwtAccessDeniedHandler.java
@@ -2,16 +2,18 @@ package fr.kmcl.unisignBACK.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.kmcl.unisignBACK.model.HttpResponse;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import static fr.kmcl.unisignBACK.constant.SecurityConstant.FORBIDDEN_MESSAGE;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static fr.kmcl.unisignBACK.constant.SecurityConstant.ACCESS_DENIED_MESSAGE;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
@@ -19,10 +21,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * @version 1.0
  * @since 01/10/2022
  */
-public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     /**
-     * Override default "Forbidden" message when accessing information while not logged in
+     * Override default "Access Denied" message when accessing information while not logged in
      * It is just to send a prettier message, but it's way cooler
      * @param request: HttpServletRequest
      * @param response: HttpServletResponse
@@ -30,10 +33,10 @@ public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
      * @throws IOException: exception
      */
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
-        HttpResponse httpResponse = new HttpResponse(FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(), FORBIDDEN_MESSAGE);
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException, ServletException {
+        HttpResponse httpResponse = new HttpResponse(UNAUTHORIZED.value(), UNAUTHORIZED, UNAUTHORIZED.getReasonPhrase().toUpperCase(), ACCESS_DENIED_MESSAGE);
         response.setContentType(APPLICATION_JSON_VALUE);
-        response.setStatus(FORBIDDEN.value());
+        response.setStatus(UNAUTHORIZED.value());
         // Take the httpServlet response (response) and stream our custom httpResponse object into it
         OutputStream outputStream = response.getOutputStream();
         ObjectMapper mapper = new ObjectMapper();
