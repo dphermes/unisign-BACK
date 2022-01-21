5f229b845effcfc1a73e9a2d4e3cb62990094038
damienpuaud <damien.puaud@gmail.com>
2022-01-21 11:38:24 +0100

    CREATE - Brute Force Attack Cache

---
+++ src/main/java/fr/kmcl/unisignBACK/service/LoginAttemptService.java
@@ -0,0 +1,63 @@
+package fr.kmcl.unisignBACK.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+/**
+ * @author KMCL (https://www.kmcl.fr)
+ * @version 1.0
+ * @since 01/10/2022
+ */
+@Service
+public class LoginAttemptService {
+    private static final int MAX_NUMBER_OF_ATTEMPTS = 5;
+    private static final int ATTEMPT_INCREMENT = 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    /**
+     * Initialize the Cache for Brute Force Attack Shield
+     */
+    public LoginAttemptService() {
+        super();
+        loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(15, MINUTES)
+                .maximumSize(100).build(new CacheLoader<String, Integer>() {
+                    public Integer load(String key) {
+                        return 0;
+                    }
+                });
+    }
+
+    /**
+     * Remove a user from the cache of Brute Force Attack Shield
+     * @param username String: user's username
+     */
+    public void evictUserFromLoginAttemptCache(String username) {
+        loginAttemptCache.invalidate(username);
+    }
+
+    /**
+     * Add a user to the cache of Brute Force Attack Shield
+     * @param username String: user's username
+     */
+    public void addUserToLoginAttemptCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = loginAttemptCache.get(username) + ATTEMPT_INCREMENT;
+        loginAttemptCache.put(username, attempts);
+    }
+
+    /**
+     * Checks if a user has exceeded the max number of attempts to log in
+     * @param username String: user's username
+     * @return boolean: has this user exceeded max attempts
+     * @throws ExecutionException: exception
+     */
+    public boolean hasExceededMaxAttempts(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAX_NUMBER_OF_ATTEMPTS;
+    }
+}
