0f4157f0beb868b59c9aea8f824d02ff2428f7ff
damienpuaud <damien.puaud@gmail.com>
2022-01-24 15:47:45 +0100

    CREATE - Create profile image directory when starting app - UnisignBackApplication

--- src/main/java/fr/kmcl/unisignBACK/UnisignBackApplication.java
+++ src/main/java/fr/kmcl/unisignBACK/UnisignBackApplication.java
@@ -5,18 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.File;
+
+import static fr.kmcl.unisignBACK.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class UnisignBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnisignBackApplication.class, args);
+		new File(USER_FOLDER).mkdirs();
 	}
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-	// My Password : VJ4dcnfk4Bf1
 
 }
