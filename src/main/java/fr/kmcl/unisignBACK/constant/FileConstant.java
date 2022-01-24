1091de87c3d3eeaba88be69cf0034c83cf9ea5f1
damienpuaud <damien.puaud@gmail.com>
2022-01-24 15:41:18 +0100

    CREATE - File Management Constants

---
+++ src/main/java/fr/kmcl/unisignBACK/constant/FileConstant.java
@@ -0,0 +1,18 @@
+package fr.kmcl.unisignBACK.constant;
+
+/**
+ * @author KMCL (https://www.kmcl.fr)
+ * @version 1.0
+ * @since 01/10/2022
+ */
+public class FileConstant {
+    public static final String USER_IMAGE_PATH = "/user/image";
+    public static final String JPG_EXTENSION = "jpg";
+    public static final String USER_FOLDER = System.getProperty("user.home") + "/unisign-BACK/user/";
+    public static final String DIRECTORY_CREATED = "Created directory for: ";
+    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile/";
+    public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
+    public static final String DOT = ".";
+    public static final String FORWARD_SLASH = "/";
+    public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
+}
