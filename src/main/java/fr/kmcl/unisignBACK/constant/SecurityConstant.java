fb205b2c7421f983b0bc2b747f6287d71b135a65
damienpuaud <damien.puaud@gmail.com>
2022-02-03 15:48:39 +0100

    MODIFY - Protect reset password endpoint with JWT

--- src/main/java/fr/kmcl/unisignBACK/constant/SecurityConstant.java
+++ src/main/java/fr/kmcl/unisignBACK/constant/SecurityConstant.java
@@ -19,10 +19,8 @@ public class SecurityConstant {
     public static final String[] PUBLIC_URLS = {
             "/api/v1/user/login",
             "/api/v1/user/register",
-            "/api/v1/user/reset-password/**",
             "/api/v1/user/image/**",
             "/user/image/**",
             "/api/v1/token/refresh/**"
     };
-    // public static final String[] PUBLIC_URLS = {"**"};
 }
