d1f3d27cb7899b8480ddfd2374a7b872d5aa5f35
damienpuaud <damien.puaud@gmail.com>
2022-02-16 11:53:49 +0100

    CREATE - Signature Service Implementation

---
+++ src/main/java/fr/kmcl/unisignBACK/service/impl/SignatureServiceImpl.java
@@ -0,0 +1,26 @@
+package fr.kmcl.unisignBACK.service.impl;
+
+import fr.kmcl.unisignBACK.model.Signature;
+import fr.kmcl.unisignBACK.repo.SignatureRepo;
+import fr.kmcl.unisignBACK.service.SignatureService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author KMCL (https://www.kmcl.fr)
+ * @version 1.0
+ * @since 01/10/2022
+ */
+@Service
+@Transactional
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class SignatureServiceImpl implements SignatureService {
+    private final SignatureRepo signatureRepo;
+
+    @Override
+    public Signature findSignatureById(Long id) {
+        return signatureRepo.findSignatureById(id);
+    }
+}
