34b494f04c430918621bf88dc9adf83b2cfc5e69
damienpuaud <damien.puaud@gmail.com>
2022-03-04 16:26:45 +0100

    MODIFY - Default Signature font

--- src/main/java/fr/kmcl/unisignBACK/constant/SignatureImplConstant.java
+++ src/main/java/fr/kmcl/unisignBACK/constant/SignatureImplConstant.java
@@ -9,10 +9,10 @@ public class SignatureImplConstant {
     public static final String SIGNATURE_LABEL_ALREADY_EXISTS = "This signature label already exists";
     public static final String NO_SIGNATURE_FOUND_WITH_LABEL = "No signature found with label ";
     public static final String SIGNATURE_DELETED_SUCCESSFULLY = "Signature deleted successfully";
-    public static final String DEFAULT_HTML_SIGNATURE = "<table id=\"i735\" style=\"box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%;\" width=\"100%\" height=\"150\">\n" +
+    public static final String DEFAULT_HTML_SIGNATURE = "<table id=\"ilob\" style=\"box-sizing: border-box; height: 150px; margin: 0 auto 10px auto; padding: 5px 5px 5px 5px; width: 100%;\" width=\"100%\" height=\"150\">\n" +
             "  <tbody style=\"box-sizing: border-box;\">\n" +
             "    <tr style=\"box-sizing: border-box;\">\n" +
-            "      <td id=\"ik57\" style=\"box-sizing: border-box; padding: 0; margin: 0; vertical-align: top;\" valign=\"top\">\n" +
+            "      <td id=\"i689\" style=\"box-sizing: border-box; padding: 0; margin: 0; vertical-align: top; font-family: Arial, Helvetica, sans-serif;\" valign=\"top\">\n" +
             "      </td>\n" +
             "    </tr>\n" +
             "  </tbody>\n" +
